9ca912086733be72166ec435dd3918ebc361e503
Antoniel Magalhães <antoniel2210@gmail.com>
2020-12-23 23:34:58 -0300

    add: save data to firestore

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Home() {
         </p>
         <div>{auth?.user?.email}</div>
         {auth.user ? (
-          <button onClick={() => auth.signinWithGithub()}>Sign In</button>
-        ) : (
           <button onClick={() => auth.signout()}>Sign out</button>
+        ) : (
+          <button onClick={() => auth.signinWithGitHub()}>Sign In</button>
         )}
       </main>
 
